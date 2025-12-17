b3588765012dd00a04f626ee57b180239d5b5785
Mager1983-bln <geracik.martin@gmail.com>
2025-12-17 21:20:20 +0100

    Update __init__.py

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,41 +1,29 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor, switch
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME
 
 cc1101_ns = cg.esphome_ns.namespace("cc1101")
 CC1101Component = cc1101_ns.class_("CC1101Component", cg.Component)
 CC1101TxSwitch = cc1101_ns.class_("CC1101TxSwitch", switch.Switch)
 
-# Basis-Schema für die Komponente
+CONF_RX_SENSOR = "rx_sensor"
+CONF_TX_SWITCH = "tx_switch"
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CC1101Component),
+    cv.Optional(CONF_RX_SENSOR): text_sensor.text_sensor_schema(CONF_NAME),
+    cv.Optional(CONF_TX_SWITCH): switch.switch_schema(CONF_NAME),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-# TextSensor-Registrierung
-TEXT_SENSOR_SCHEMA = text_sensor.text_sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(CC1101Component),
-})
+    if CONF_RX_SENSOR in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_RX_SENSOR])
+        cg.add(var.rx_sensor.set_parent(sens))
 
-@text_sensor.register("cc1101", TEXT_SENSOR_SCHEMA)
-async def text_sensor_to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
-    sens = await text_sensor.new_text_sensor(config)
-    cg.add(parent.rx_sensor.set_parent(sens))
-
-# Switch-Registrierung
-SWITCH_SCHEMA = switch.switch_schema(CC1101TxSwitch).extend({
-    cv.GenerateID(): cv.declare_id(CC1101Component),
-})
-
-@switch.register("cc1101", SWITCH_SCHEMA)
-async def switch_to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
-    sw = await switch.new_switch(config)
-    cg.add(sw.set_parent(parent))
-
-
+    if CONF_TX_SWITCH in config:
+        sw = await switch.new_switch(config[CONF_TX_SWITCH])
+        cg.add(sw.set_parent(var))
